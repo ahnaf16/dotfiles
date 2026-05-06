1c28e16f9a090b366830b4d690f49f9516ede611
Ahnaf Sakil <55395335+ahnaf16@users.noreply.github.com>
2026-05-06 19:52:36 +0600

    added .zsh

---
+++ dot_zsh/functions.zsh
@@ -0,0 +1,13 @@
+# Colormap
+function colormap() {
+  for i in {0..255}; do print -Pn "%K{$i}  %k%F{$i}${(l:3::0:)i}%f " ${${(M)$((i%6)):#3}:+$'\n'}; done
+}
+
+function yy() {
+	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")"
+	yazi "$@" --cwd-file="$tmp"
+	if cwd="$(cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
+		cd -- "$cwd"
+	fi
+	rm -f -- "$tmp"
+}
