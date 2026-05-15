c38c823d4e82d639bc1b8a9aa54f2c8b7e76319a
Ahnaf Sakil <55395335+ahnaf16@users.noreply.github.com>
2026-05-15 11:57:30 +0600

    removed cz

--- dot_config/folder_templates/fastapi_init/app/main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-from app.core.config import settings
-
-app = FastAPI(title=settings.PROJECT_NAME)
-
-
-@app.get("/")
-def root():
-    return {"message": "<FTName | capitalcase> API is running"}
